d129280320ab36f0b14a204a6d3f546fb0d356c7
Yohan Wiputra <yowiputra@gmail.com>
2017-09-04 18:02:32 -0700

    cleaned up navbar code

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, Grid } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
   render(){
     return(
       <Navbar inverse fixedTop collapseOnSelect>
-        <Grid>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="/">tasq</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <NavItem eventKey={1}>
-                <Link to={this.props.link}>Switch Views</Link>
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Grid>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">tasq</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem key={1}>
+              <Link to={this.props.link}>Switch Views</Link>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     )
   }
