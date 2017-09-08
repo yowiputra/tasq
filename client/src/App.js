e364f91be402e8c71bac29cc9389511a8063d41b
yowiputra <yowiputra@gmail.com>
2017-09-08 11:18:32 -0700

    removed jumbotron and added top-margin to increase responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Jumbotron, Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 import orderBy from 'lodash/orderBy';
 import './styles/app.css';
 import NavBar from './components/NavBar';
@@ -93,32 +93,30 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar link="/tableview" />
-        <Jumbotron>
-          <div className="container">
-            <div className="list-holder">
-              <Grid>
-                <Row className="show-grid">
-                  <Col xs={6} md={4}>
-                    <UserList
-                      userData={this.state.users}
-                      taskUserId={this.state.taskUserId}
-                      getTasks={this.getTasks}
-                      postUser={this.postUser}
-                      deleteUser={this.deleteUser} />
-                  </Col>
-                  <Col xs={12} md={8}>
-                    <TaskList
-                      taskData={this.state.tasks}
-                      taskUserId={this.state.taskUserId}
-                      postTask={this.postTask}
-                      completeTask={this.completeTask}
-                      deleteTask={this.deleteTask} />
-                  </Col>
-                </Row>
-              </Grid>
-            </div>
+        <div className="container">
+          <div className="list-holder">
+            <Grid>
+              <Row className="show-grid">
+                <Col xs={6} md={4}>
+                  <UserList
+                    userData={this.state.users}
+                    taskUserId={this.state.taskUserId}
+                    getTasks={this.getTasks}
+                    postUser={this.postUser}
+                    deleteUser={this.deleteUser} />
+                </Col>
+                <Col xs={12} md={8}>
+                  <TaskList
+                    taskData={this.state.tasks}
+                    taskUserId={this.state.taskUserId}
+                    postTask={this.postTask}
+                    completeTask={this.completeTask}
+                    deleteTask={this.deleteTask} />
+                </Col>
+              </Row>
+            </Grid>
           </div>
-        </Jumbotron>
+        </div>
       </div>
     );
   }
